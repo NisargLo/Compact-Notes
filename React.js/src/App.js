94b8fe1b51ca99ce859aca2185c75f51c9c7f395
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-09-28 00:08:30 +0530

    Update App.js

--- React.js/src/App.js
+++ React.js/src/App.js
@@ -13,7 +13,7 @@ function App() {
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
 						<Route path='/About' element={<About />} />
-						<Route path='/Contact' element={<Contact />} /> 
+						<Route path='/Contact' element={<Contact />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
