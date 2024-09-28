edc94544830c1830c433ae5b583b81c90aff0c94
Nisarg Lo <nisarg31305@gmail.com>
2024-09-28 16:00:01 +0530

    28-9-24

--- React.js/src/Pages/Home.jsx
+++ React.js/src/Pages/Home.jsx
@@ -1,35 +1,106 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import paris_image from '../Images/Paris.jpg';
+import sydney_image from '../Images/Sydney.jpg';
+import melbourne_image from '../Images/Melbourne.jpg';
+import tiger_image from '../Images/Tiger.jpg';
+import lion_image from '../Images/Lion.jpg';
+import '../Styles/Home_Style.css';
 
 function Home() {
-    const [notes, setNotes] = useState([]);
+    const notes = [
+        {
+            name: 'Paris',
+            description: 'Paris, the capital of France, is renowned for its art, fashion, and culture. Iconic landmarks like the Eiffel Tower and the Louvre Museum attract millions of visitors each year, making it a symbol of romance and elegance.',
+            image: paris_image
+        },
+        {
+            name: 'Tiger',
+            description: 'Tigers are magnificent big cats known for their striking orange fur with black stripes, making them one of the most recognizable animals in the world. They are solitary hunters, primarily found in forests and grasslands across Asia, and are critically endangered due to habitat loss and poaching.',
+            image: tiger_image
+        },
+        {
+            name: 'Sydney',
+            description: 'Sydney, located on Australia’s east coast, is famous for its stunning harbor, iconic Sydney Opera House, and beautiful beaches like Bondi Beach. The city offers a mix of outdoor adventures and urban experiences, making it a popular destination for tourists and locals alike.',
+            image: sydney_image
+        },
+        {
+            name: 'Melbourne',
+            description: 'Melbourne is a vibrant city in Australia known for its rich cultural scene, diverse cuisine, and sporting events. It boasts a blend of Victorian architecture and modern design, along with numerous galleries, theaters, and festivals, earning it the title of Australia’s cultural capital.',
+            image: melbourne_image
+        },
+        {
+            name: 'Lion',
+            description: 'Lions, often referred to as the "king of the jungle," are social big cats that live in prides, primarily found in sub-Saharan Africa and a small population in India. Known for their powerful roar and majestic mane, they play a crucial role in their ecosystem as top predators.',
+            image: lion_image
+        },
+    ];
 
-    useEffect(() => {
-        fetch('https://66f37eed71c84d805878e31d.mockapi.io/Notes')
-            .then(res => res.json())
-            .then(data => setNotes(data))
-            .catch(error => console.log('Error: ', error));
-    }, []);
+    const [modalShow, setModalShow] = useState(false);
+    const [selectedNote, setSelectedNote] = useState(null);
+
+    const handleReadClick = (note) => {
+        setSelectedNote(note);
+        setModalShow(true);
+    };
 
     return (
         <>
             <center>
-                <div className="d-flex justify-content-center row px-4">
-                    {notes.map(note => (
-                        <div key={note.id} className="col-4 mb-4">
-                            <div className="card">
-                                <div className="card-header placeholder-glow">{note.name}</div>
-                                <img src={note.avatar} className="card-img-top placeholder-glow" alt={note.name} />
-                                <div className="card-body">
-                                    <h5 className="card-title placeholder-glow">{note.name}</h5>
-                                    <p className="card-text placeholder-glow">{note.description}</p>
-                                    <p className="card-text placeholder-glow"><small className="text-muted">ID: {note.id}</small></p>
+                <div className="d-flex justify-content-center row">
+                    {notes.map((note, index) => (
+                        <div key={index} className="col-auto mb-4" style={{ maxWidth: '30rem' }}>
+                            <div className="card d-flex justify-content-center align-content-center border border-2 border-dark-subtle">
+                                <div className="card-header placeholder-glow text-truncate px-2 myHeader border-2 border-dark-subtle">{note.name}</div>
+                                <div className="placeholder-glow m-2">
+                                    <img src={note.image} className="card-img-top placeholder-glow rounded img-fluid myimg" alt={note.name} />
+                                </div>
+                                <div className="card-body mb-2">
+                                    <h5 className="card-title placeholder-glow text-truncate myTitle h-100">{note.name}</h5>
+                                    <p className="card-text placeholder-glow text-truncate myContent">{note.description}</p>
+                                </div>
+                                <div className="d-flex justify-content-center align-content-center mb-2">
+                                    <div className="mx-auto">
+                                        <button className="btn btn-primary bg-gradient rounded-3 py-2 px-3" onClick={() => handleReadClick(note)}>
+                                            <p className='h5'><i class="fa-solid fa-book"></i> Read</p>
+                                        </button>
+                                    </div>
+                                    <div className="mx-auto">
+                                        <button className="btn btn-warning bg-gradient rounded-3 py-2 px-3"><p className='h5'><i class="fa-solid fa-pen-to-square"></i> Edit</p></button>
+                                    </div>
+                                    <div className="mx-auto">
+                                        <button className="btn btn-danger bg-gradient rounded-3 py-2 px-3"><p className='h5'><i class="fa-solid fa-trash"></i> Delete</p></button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     ))}
                 </div>
             </center>
+            {modalShow && selectedNote && (
+                <Modal note={selectedNote} onClose={() => setModalShow(false)} />
+            )}
         </>
+    );
+}
+
+function Modal({ note, onClose }) {
+    return (
+        <div className="modal fade show" style={{ display: 'flex' }}>
+            <div className="modal-dialog modal-fullscreen" role="document">
+                <div className="modal-content ">
+                    <div className="text-center">
+                        <p className="modal-header modal-title display-5">{note.name}</p>
+                    </div>
+                    <div className="modal-body d-flex flex-column align-items-center">
+                        <img src={note.image} className="img-fluid rounded-3 my-1" alt={note.name} style={{ maxHeight: '20rem', width: 'auto' }} />
+                        <p className="mt-3" style={{ fontSize: '1.5rem' }}>{note.description}</p>
+                    </div>
+                    <div className="modal-footer px-4">
+                        <button type="button" className="btn btn-secondary" onClick={onClose}><p className="h5">Close</p></button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- React.js/src/Pages/Layout.jsx
+++ React.js/src/Pages/Layout.jsx
@@ -5,24 +5,22 @@ function Layout() {
     return (
         <div className='container-fluid'>
             <div className='mb-4 sticky-top'>
-                <nav class="navbar bg-primary bg-gradient">
+                <nav class="navbar bg-secondary bg-gradient">
                     <a href="http://localhost:3000" class="navbar-brand"><i class="text-light fa-solid fa-note-sticky h1 ms-4" /></a>
-                    <Link to="/" className="h2 mx-5 text-decoration-none text-light">Home</Link>
-                    <Link to="/About" className="h2 mx-5 text-decoration-none text-light">About</Link>
-                    <Link to="/Contact" className="h2 mx-5 text-decoration-none text-light">Contact</Link>
+                    <Link to="/" className="h2 ms-5 me-auto text-decoration-none text-light">Home</Link>
+                    <Link to="/About" className="h2 me-auto text-decoration-none text-light">About</Link>
+                    <Link to="/Contact" className="h2 me-auto text-decoration-none text-light">Contact</Link>
                     <form class="d-flex ms-auto" role="search">
                         <input class="form-control m-3 mysearchbar bg-light border-secondary" type="search" placeholder="Search Notes" aria-label="Search" />
                         <button class="btn btn-light p-2 px-3 my-3 me-4" type="submit">Search</button>
                     </form>
                 </nav>
             </div>
-            <div className='mb-3'>
+            <div className='my-3'>
                 <Outlet />
             </div>
-            <div className='custom-fixed bg-secondary-subtle border border-secondary rounded ms-3 me-3'>
-                <button className="btn btn-success bg-gradient my-3 mx-auto rounded-3 py-2 px-3 me-4 ms-4"><p className='h4'>Add</p></button>
-                <button className="btn btn-warning bg-gradient my-3 mx-auto rounded-3 py-2 px-3 me-4"><p className='h4'>Edit</p></button>
-                <button className="btn btn-danger bg-gradient my-3 mx-auto rounded-3 py-2 px-3 me-4"><p className='h4'>Delete</p></button>
+            <div className='custom-fixed bg-light bg-gradient rounded-3 ms-3 me-3 p-1'>
+                <button className="btn btn-success bg-gradient rounded-3 p-3"><p className='h4'><i class="fa-solid fa-plus text-light-subtle"></i> Add Note</p></button>
             </div>
         </div>
     );
